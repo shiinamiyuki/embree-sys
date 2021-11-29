7088b13c09ba19af0a6f041e82a7123d3698b8c6
shiinamiyuki <miyuki@mail.shiinamiyuki.com>
2021-11-29 06:20:32 -0500

    good

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,10 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 include!("binding.rs");
+
+mod test {
+    #[test]
+    fn new_device() {
+        unsafe { crate::rtcNewDevice(std::ptr::null()); }
+    }
+}

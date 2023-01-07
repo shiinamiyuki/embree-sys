4d96397b5d7cd849547f861c59b4e67908ac1065
Xiaochun Tong <xtong@uwaterloo.ca>
2023-01-07 09:38:39 +0000

    good

--- build.rs
+++ build.rs
@@ -1,62 +1,19 @@
 use std::env;
 use std::io::Result;
 use std::process::Command;
-fn prebuilt_filename() -> Option<(&'static str, &'static str)> {
-    if cfg!(target_arch = "x86_64") {
-        if cfg!(target_os = "linux") {
-            return Some(("https://github.com/embree/embree/releases/download/v3.13.4/embree-3.13.4.x86_64.linux.tar.gz",
-            "embree.tar.gz"));
-        }
-        if cfg!(target_os = "macos") {
-            return Some( ("https://github.com/embree/embree/releases/download/v3.13.4/embree-3.13.4.x86_64.macosx.zip",
-            "embree.zip"));
-        }
-        if cfg!(target_os = "windows") {
-            return Some( ("https://github.com/embree/embree/releases/download/v3.13.4/embree-3.13.4.x64.vc14.windows.zip",
-            "embree.zip"));
-        }
-    }
-    None
-}
-fn source_filename() -> (&'static str, &'static str) {
-    (
-        "https://github.com/embree/embree/archive/refs/tags/v3.13.4.zip",
-        "embree.zip",
-    )
-}
+
 fn build_embree() -> Result<String> {
     let out_dir = env::var("OUT_DIR").unwrap();
-    let out_dir = out_dir.clone() + &"/embree";
-    if std::path::Path::new(&out_dir).exists() {
-        return Ok(out_dir);
-    }
-    let prebuilt = prebuilt_filename();
-    let source = source_filename();
 
-    let (url, file) = if let Some(prebuilt) = prebuilt {
-        prebuilt
-    } else {
-        source
-    };
+    cmake::Config::new("embree")
+        .define("CMAKE_BUILD_TYPE", "Release")
+        .define("EMBREE_ISPC_SUPPORT", "OFF")
+        .define("EMBREE_TASKING_SYSTEM", "INTERNAL")
+        .define("EMBREE_TUTORIALS", "OFF")
+        .define("EMBREE_STATIC_LIB", "OFF")
+        .generator("Ninja")
+        .build();
 
-    Command::new("curl")
-        .args(["-L", url, "--output", file])
-        .output()
-        .unwrap();
-    dbg!(&out_dir);
-    std::fs::create_dir_all(&out_dir).unwrap();
-    Command::new("tar")
-        .args(["-zxvf", file, "-C", &out_dir, "--strip-components=1"])
-        .output()
-        .unwrap();
-    if let None = prebuilt {
-        cmake::Config::new(&out_dir)
-            .define("CMAKE_BUILD_TYPE", "Release")
-            .define("EMBREE_ISPC_SUPPORT", "OFF")
-            .define("EMBREE_TASKING_SYSTEM", "INTERNAL")
-            .generator("Ninja")
-            .build();
-    }
     Ok(out_dir)
 }
 fn gen(out_dir: &String) -> Result<()> {
@@ -74,44 +31,35 @@ fn gen(out_dir: &String) -> Result<()> {
     Ok(())
 }
 
-// fn get_output_path() -> PathBuf {
-//     //<root or manifest path>/target/<profile>/
-//     let manifest_dir_string = env::var("CARGO_MANIFEST_DIR").unwrap();
-//     let build_type = env::var("PROFILE").unwrap();
-//     let path = Path::new(&manifest_dir_string)
-//         .join("target")
-//         .join(build_type);
-//     return PathBuf::from(path);
-// }
-
 fn main() -> Result<()> {
     let out_dir = build_embree().unwrap();
     gen(&out_dir)?;
     let out_dir = env::var("OUT_DIR").unwrap();
-    println!("cargo:rustc-link-search=native={}/embree/bin/", out_dir);
-    println!("cargo:rustc-link-search=native={}/embree/lib/", out_dir);
+    println!("cargo:rustc-link-search=native={}/bin/", out_dir);
+    println!("cargo:rustc-link-search=native={}/lib/", out_dir);
     println!("cargo:rustc-link-lib=dylib=embree3");
 
     let out_dir = if cfg!(target_os = "windows") {
-        out_dir.clone() + &"/embree/bin"
+        out_dir.clone() + &"/bin"
     } else {
-        out_dir.clone() + &"/embree/lib"
+        out_dir.clone() + &"/lib"
     };
-
+    let mut count = 0;
     for entry in std::fs::read_dir(out_dir)? {
         let entry = entry?;
         let path = entry.path();
         if path.extension().is_some()
             && (path.extension().unwrap() == "dll" || path.extension().unwrap() == "so")
         {
-            // let target_dir = get_output_path();
             let comps: Vec<_> = path.components().collect();
-            let dest = std::path::PathBuf::from_iter(comps[..comps.len() - 6].iter())
+            let dest = std::path::PathBuf::from_iter(comps[..comps.len() - 5].iter())
                 .join(path.file_name().unwrap());
             println!("{:?}", path);
             println!("{:?}", dest);
             std::fs::copy(path, dest).unwrap();
+            count += 1;
         }
     }
+    assert!(count > 0, "No dlls or so files found");
     Ok(())
 }

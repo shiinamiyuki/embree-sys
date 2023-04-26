63fe001d49ef3ac8bd3bbfe9819a798e5c6e9e4d
Xiaochun Tong <xtong@uwaterloo.ca>
2023-04-26 11:29:20 -0400

    bad

--- build.rs
+++ build.rs
@@ -173,7 +173,11 @@ fn build_embree_from_source() -> Result<()> {
     println!("cargo:rustc-link-lib=dylib=embree4");
     let out_dir = PathBuf::from(out_dir);
     let comps: Vec<_> = out_dir.components().collect();
-    let shift = if cfg!(feature="hack_copy_dll") { 7}else {  0};
+    let shift = if cfg!(feature = "hack_copy_dll") {
+        8
+    } else {
+        0
+    };
     let dst_dir = PathBuf::from_iter(comps[..comps.len() - 3 - shift].iter());
 
     let get_dll_dir = |subdir| {
@@ -203,7 +207,11 @@ fn prebuild() -> Result<()> {
     let out_dir = PathBuf::from(out_dir);
     let out_dir = fs::canonicalize(out_dir).unwrap();
     let comps: Vec<_> = out_dir.components().collect();
-    let shift = if cfg!(feature="hack_copy_dll") { 7}else {  0};
+    let shift = if cfg!(feature = "hack_copy_dll") {
+        8
+    } else {
+        0
+    };
     let out_dir = PathBuf::from_iter(comps[..comps.len() - 3 - shift].iter());
 
     copy_dlls(&get_dll_dir("lib"), &out_dir);

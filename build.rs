858c72c47bee0ca54dd7202814abb3ffa5483e1c
Zheng Shaokun <Mango@Live.cn>
2023-08-27 15:22:53 +0800

    Update build.rs

--- build.rs
+++ build.rs
@@ -46,9 +46,11 @@ fn build_embree() -> Result<String> {
             } else {
                 "NEON2X"
             })
-        .define("CMAKE_C_FLAGS", "\"\"")
-        .define("CMAKE_CXX_FLAGS", "\"\"")
-        .define("CMAKE_ASM_FLAGS", "\"\"")
+        .define("CMAKE_C_FLAGS", "")
+        .define("CMAKE_CXX_FLAGS", "")
+        .define("CMAKE_ASM_FLAGS", "")
+        .define("CMAKE_C_COMPILER", "")
+        .define("CMAKE_CXX_COMPILER", "")
         .generator(generator)
         .build();
 
